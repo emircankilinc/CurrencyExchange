e7e76e302f434954f1f4cc5960633c766dc7eccc
emircankilinc <emircan_kilinc@hotmail.com>
2021-08-29 17:59:09 +0300

    Update IndexController with new fetures and functionalities.

--- src/main/java/de/c24/finacc/klt/web/controller/IndexController.java
+++ src/main/java/de/c24/finacc/klt/web/controller/IndexController.java
@@ -1,8 +1,21 @@
 package de.c24.finacc.klt.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import de.c24.finacc.klt.model.CurrencyModel;
+import de.c24.finacc.klt.service.intf.ICurrencyExchangeCallService;
+import de.c24.finacc.klt.service.request.RequestCurrencyExchange;
+import de.c24.finacc.klt.service.response.ResponseCurrencyExchange;
 
 /**
  * IndexController
@@ -10,17 +23,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    /**
-     * Index endpoint to show the index page
-     *
-     * @param model Spring's view model
-     * @return view name
-     */
-    @GetMapping({"/", "/index"})
-    public String index(Model model) {
-        model.addAttribute("title", "Karten&Konten KLT");
-        model.addAttribute("welcome", "Welcome to Check24");
-        model.addAttribute("applicationTitle", "Check24 Currency Converter");
-        return "index";
-    }
+	//Injection of currency exchange service
+	@Autowired
+	ICurrencyExchangeCallService currencyExchangeCallService;
+
+	/**
+	 * Index and currency attributes and properties
+	 *
+	 * @param model Spring's view model
+	 * @return view name
+	 */
+	@GetMapping({ "/", "/converter" })
+	public String index(Model model) {
+		model.addAttribute("title", "Karten&Konten KLT");
+		model.addAttribute("welcome", "Welcome to Check24");
+		model.addAttribute("applicationTitle", "Check24 Currency Converter");
+		model.addAttribute("currencyModel", currencyExchangeCallService.getCurrencyModel());
+		return "index";
+	}
+
+
+	/**
+	 * Call and set the result currency source to target i.e. EUR TO TRY
+	 *
+	 * @param currencyModel CurrencyModel
+	 * @param model Model 
+	 * @param request HttpServletRequest
+	 * @return ResponseEntity<ResponseCurrencyExchange>
+	 */
+	@RequestMapping(value = "/convert", method = RequestMethod.POST)
+	public ResponseEntity<ResponseCurrencyExchange> convertCurrencySourceToTarget(
+			@ModelAttribute CurrencyModel currencyModel, Model model, HttpServletRequest request) {
+
+		HttpStatus httpStatus = HttpStatus.OK;
+
+		// setting currencyModel from html with js.
+		model.addAttribute("currencyModel", currencyModel);
+
+		// Currency Exchange service is called with selected currencies with amount.
+		ResponseCurrencyExchange responseCurrencyExchange = currencyExchangeCallService
+				.getConvertedValue(new RequestCurrencyExchange(currencyModel.getSourceCurrency(),
+						currencyModel.getTargetCurrency(), currencyModel.getSourceAmount()));
+
+		return new ResponseEntity<ResponseCurrencyExchange>(responseCurrencyExchange, httpStatus);
+	}
 }
