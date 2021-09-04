23a4aafd3b85cd6a2faec1d8b4332ceade1fb320
emircankilinc <emircan_kilinc@hotmail.com>
2021-09-04 17:37:21 +0300

    Add service test.

--- src/test/java/de/c24/finacc/klt/rest/RestServiceTest.java
+++ src/test/java/de/c24/finacc/klt/rest/RestServiceTest.java
@@ -2,22 +2,33 @@ package de.c24.finacc.klt.rest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Map;
+import de.c24.finacc.klt.service.intf.ICurrencyExchangeCallService;
+import de.c24.finacc.klt.service.request.RequestCurrencyExchange;
+import de.c24.finacc.klt.service.response.ResponseCurrencyExchange;
+import de.c24.finacc.klt.util.Currency;
 
 /**
  * RestServiceTest
  */
+
+@SpringBootTest
 class RestServiceTest {
 
-    private RestService restService = new RestService();
+	@Autowired
+	private ICurrencyExchangeCallService currencyExchangeCallService;
 
-    @Test
-    @DisplayName("Simple test")
-    void testit() {
-        Map<String, String> result = restService.test();
-        assertThat(result).isNotEmpty();
-    }
+	@Test
+	@DisplayName("Simple test")
+	void testit() {
+		RequestCurrencyExchange request = new RequestCurrencyExchange(Currency.EUR, Currency.TRY, new BigDecimal(10));
+		ResponseCurrencyExchange result = currencyExchangeCallService.getConvertedValue(request);
+		assertThat(result).isNotNull();
+	}
 }
